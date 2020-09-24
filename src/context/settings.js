5399a1317a3c998f67e89d495b7de2f1d9768e69
seanjmurray <sean.j.murray1@gmail.com>
2020-09-24 08:30:09 -0700

    Work on context

---
+++ src/context/settings.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+export const SettingsContext = React.createContext()
+
+const SettingsProvider = (props) => {
+    const [showComplete, setShowComplete] = useState(true)
+    const [itemCount, setItemCount] = useState(5)
+    const [sortBy, setSort] = useState('difficulty')
+
+    const toggleComplete = () => { setShowComplete(!showComplete) }
+
+    const dispCount = (num) => {
+        setItemCount(num)
+    }
+
+    const setSortBy = (str) => {
+        setSort(str)
+    }
+
+    const state = {
+        showComplete,
+        itemCount,
+        sortBy,
+        toggleComplete,
+        dispCount,
+        setSortBy
+    }
+
+    return (
+        <SettingsContext.Provider value={state} >
+            {props.children}
+        </SettingsContext.Provider>
+
+    )
+}
+
+export default SettingsProvider
